02f526027a0df5c8ae6b3733704ece5f8c9fa447
mohamedmosilhy <14712020100557@stud.cu.edu.eg>
2023-04-07 00:27:08 +0200

    add the logic to the calculator

--- jscode.js
+++ jscode.js
@@ -19,29 +19,64 @@ let root = document.querySelector('.root');
 let input = document.querySelector('input');
 let grid_element = document.querySelectorAll('.input')
 
+let input_string;
+
 grid_element.forEach(element => {
     if(element === power){
         element.addEventListener('click',function(){
-            input.value += element.textContent.slice(1) ;
+            input.value += element.textContent.slice(1);
+            input_string += "**2"
         });
         return;  
     }
     element.addEventListener('click',function(){
+        switch (element) {
+            case plus:
+                input_string += "+";
+                break;
+            case mult:
+                input_string += "*"
+                break;
+            case div:
+                input_string += "/"
+                break;
+            case percent:
+                input_string += "%"
+                break;
+            case dot:
+                input_string += "."
+                break;
+            case minus:
+                input_string += "-"
+                break;
+            case open_bracket:
+                input_string += "("
+                break;  
+            case closed_bracket:
+                input_string += ")"
+                break;  
+            case root:
+                input_string += "**(1/2)"
+                break;
+            default:
+                input_string += element.textContent
+        }
         input.value += element.textContent;
     });
 });
 
 ac.addEventListener('click',()=>{
     input.value = '';
+    input_string = ""
 });
 
 del.addEventListener('click',()=>{
     input.value = input.value.slice(0,input.value.length - 1);
+    input_string -= input_string
 });
 
 
-equal.addEventListener('click',()=>{
-    input.value = eval(input.value);
-});
-
-
+equal.addEventListener('click', function() {
+    input.value = eval(input_string)
+    input_string = ""
+})
